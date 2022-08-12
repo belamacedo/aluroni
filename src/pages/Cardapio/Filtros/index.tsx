2de9883b8be19bce7718deb74bc947bc492186ff
isabelamacedo <isabelacb.macedo@gmail.com>
2022-08-12 19:49:55 -0300

    feat: fazendo os filtros funcionarem

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,16 +1,30 @@
+import React from 'react';
+import { OperationCanceledException } from 'typescript';
 import filtros from './filtros.json';
 import styles from './Filtros.module.scss';
+import classNames from 'classnames';
 
 type IOpcao = typeof filtros[0];
 
-export default function Filtros() {
-  function selecionarFiltro(item: IOpcao) {}
+interface Props {
+  filtro: number | null;
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+export default function Filtros({ filtro, setFiltro }: Props) {
+  function selecionarFiltro(item: IOpcao) {
+    if (filtro === item.id) return setFiltro(null);
+    return setFiltro(item.id);
+  }
 
   return (
     <div className={styles.filtros}>
       {filtros.map((item) => (
         <button
-          className={styles.filtros__filtro}
+          className={classNames({
+            [styles.filtros__filtro]: true,
+            [styles['filtros__filtro--ativo']]: filtro === item.id,
+          })}
           key={item.id}
           onClick={() => selecionarFiltro(item)}
         >

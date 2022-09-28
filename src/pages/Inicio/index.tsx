5f7cb6947ea05d5402963e786f64ee74b2358f29
isabelamacedo <isabelacb.macedo@gmail.com>
2022-09-28 17:35:24 -0300

    feat: terminando a tela de inicio

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -2,30 +2,33 @@ import cardapio from 'data/cardapio.json';
 import { Key } from 'react';
 import styles from './Inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from '../../assets/nossa_casa.png';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
   pratosRecomendados = pratosRecomendados
     .sort(() => 0.5 - Math.random())
     .splice(0, 3);
+
   return (
     <section>
       <h3 className={stylesTema.titulo}>Recomendações da cozinha</h3>
       <div className={styles.recomendados}>
-        {pratosRecomendados.map(
-          (item: {
-            id: Key | null | undefined;
-            photo: string | undefined;
-            title: string | undefined;
-          }) => (
-            <div key={item.id} className={styles.recomendado}>
-              <div className={styles.recomendado__imagem}>
-                <img src={item.photo} alt={item.title} />
-              </div>
-              <button className={styles.recomendado__botao}>Ver mais</button>
+        {pratosRecomendados.map((item) => (
+          <div key={item.id} className={styles.recomendado}>
+            <div className={styles.recomendado__imagem}>
+              <img src={item.photo} alt={item.title} />
             </div>
-          )
-        )}
+            <button className={styles.recomendado__botao}>Ver mais</button>
+          </div>
+        ))}
+        <h3 className={stylesTema.titulo}> Nossa casa </h3>
+        <div className={styles.nossaCasa}>
+          <img src={nossaCasa} alt='Casa do aluroni' />
+          <div className={styles.nossaCasa__endereco}>
+            Rua Vergueiro, 3185 <br /> <br /> Vila Mariana - SP
+          </div>
+        </div>
       </div>
     </section>
   );

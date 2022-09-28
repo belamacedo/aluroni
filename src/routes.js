609a5794ee0bc98ce6790bb38d99dd731a892314
isabelamacedo <isabelacb.macedo@gmail.com>
2022-09-28 18:41:02 -0300

    feat: criando o NotFound

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './pages/Inicio';
 import Cardapio from './pages/Cardapio';
 import Sobre from './pages/Sobre';
+import NotFound from './pages/NotFound';
 import Menu from './components/Menu';
+import Footer from './components/Footer';
 import PaginaPadrao from './components/PaginaPadrao';
 
 export default function AppRouter() {
@@ -17,7 +19,10 @@ export default function AppRouter() {
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
           </Route>
+          <Route path='*' element={<NotFound />} />
         </Routes>
+
+        <Footer />
       </Router>
     </main>
   );

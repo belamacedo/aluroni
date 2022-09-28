760e5f64b2a713cdff2206c36b958ac63f40ab46
isabelamacedo <isabelacb.macedo@gmail.com>
2022-09-28 16:35:58 -0300

    feat: adicionando pagina padrao

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './pages/Inicio';
 import Cardapio from './pages/Cardapio';
 import Menu from './components/Menu';
+import PaginaPadrao from './components/PaginaPadrao';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />} />
+          <Route index element={<Inicio />} />
+          <Route path='cardapio' element={<Cardapio />}></Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }

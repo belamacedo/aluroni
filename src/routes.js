1ad279ce7173ce093312d99b4c4eeee84c6013ce
isabelamacedo <isabelacb.macedo@gmail.com>
2022-09-28 18:53:00 -0300

    feat: criando a rota prato

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,14 @@ import Inicio from './pages/Inicio';
 import Cardapio from './pages/Cardapio';
 import Sobre from './pages/Sobre';
 import NotFound from './pages/NotFound';
+import Prato from './pages/Prato';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
 import PaginaPadrao from './components/PaginaPadrao';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className='container'>
       <Router>
         <Menu />
         <Routes>
@@ -18,6 +19,7 @@ export default function AppRouter() {
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
+            <Route path='prato/:id' element={<Prato />} />
           </Route>
           <Route path='*' element={<NotFound />} />
         </Routes>

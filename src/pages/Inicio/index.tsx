fd1f21d583bd4251fff5743e22504c1b91d8027a
isabelamacedo <isabelacb.macedo@gmail.com>
2022-09-28 16:08:46 -0300

    feat: reaproveitando com react-router

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,4 +1,3 @@
-import Menu from 'components/Menu';
 import cardapio from 'data/cardapio.json';
 import { Key } from 'react';
 import styles from './Inicio.module.scss';
@@ -10,7 +9,6 @@ export default function Inicio() {
     .splice(0, 3);
   return (
     <section>
-      <Menu />
       <h3 className={styles.titulo}>Recomendações da cozinha</h3>
       <div className={styles.recomendados}>
         {pratosRecomendados.map(
